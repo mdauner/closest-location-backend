c543db73b40ade6434b012a353e4c14135a05c33
Michael Dauner <mdauner@gmail.com>
2019-04-09 11:12:22 -0500

    Geocode locations in endpoint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const { getLocation } = require('./utils.js')
 
 const app = express()
 
 app.get('/closest-locations', async (req, res) => {
   const { locations: locationNames } = req.query
 
-  return res.send(locationNames)
+  const locations = await Promise.all(locationNames.map(getLocation))
+  const locationsByName = {}
+
+  locationNames.forEach((name, index) => {
+    // eslint-disable-next-line prefer-destructuring
+    locationsByName[name] = locations[index]
+  })
+
+  return res.send(locationsByName)
 })
 
 module.exports = app
