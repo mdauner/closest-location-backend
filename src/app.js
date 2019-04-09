b9c849463820ed711561b340705e0e7bb5e2900b
Michael Dauner <mdauner@gmail.com>
2019-04-09 11:07:18 -0500

    Add endpoint and read query params

--- src/app.js
+++ src/app.js
@@ -2,4 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/closest-locations', async (req, res) => {
+  const { locations: locationNames } = req.query
+
+  return res.send(locationNames)
+})
+
 module.exports = app
