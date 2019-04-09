3240ab9d4ad96af9fe9b350ed4d2a1657c16d04d
Michael Dauner <mdauner@gmail.com>
2019-04-09 11:38:05 -0500

    Handle errors

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ const app = express()
 app.get('/closest-locations', async (req, res) => {
   const { locations: locationNames } = req.query
 
+  if (!Array.isArray(locationNames)) {
+    return res
+      .status(400)
+      .send('two location names are required to calculate closest locations')
+  }
+
   const locations = await Promise.all(
     locationNames.map(getLocationFromCacheOrGeocode)
   )
@@ -19,6 +25,12 @@ app.get('/closest-locations', async (req, res) => {
     // eslint-disable-next-line prefer-destructuring
     locationsByName[name] = locations[index]
   })
+
+  if (Object.keys(locationsByName).length < 2) {
+    return res
+      .status(400)
+      .send('two valid locations are required to calculate closest locations')
+  }
 
   const data = Object.keys(locationsByName).map(name => ({
     location: name,
