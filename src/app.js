079527dd42e3bd5dfa2ed7209b9e3aced4f66ef9
Michael Dauner <mdauner@gmail.com>
2019-04-09 20:34:08 -0500

    Enable all cors requests

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const geolib = require('geolib')
 const { getLocationFromCacheOrGeocode } = require('./utils.js')
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.static('public'))
 
